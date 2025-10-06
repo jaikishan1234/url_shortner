1dd8d29a7a99ed8e256bd73dd5ff9e2d4f68a93f
jai <jaikishannayak@gmail.com>
2025-10-06 13:52:50 +0530

    Project Started

---
+++ BACKEND/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import { nanoid } from "nanoid";
+import connectDB from "./src/config/monogo.config.js";
+import urlSchema from "./src/models/short_url.model.js"
+import short_url from "./src/routes/short_url.route.js"
+import { redirectFromShortUrl } from "./src/controller/short_url.controller.js";
+import { errorHandler } from "./src/utils/errorHandler.js";
+import cors from "cors"
+
+// Correct way:
+dotenv.config({ path: "./.env" });
+
+const app = express();
+app.use(cors())
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/create",short_url)
+app.get("/:id",redirectFromShortUrl)
+
+app.use(errorHandler)
+
+app.listen(3000, () => {
+    connectDB();
+    console.log("Server is running on http://localhost:3000");
+});
