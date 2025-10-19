6223d3001c19366ac920ed36d1304dafa9c92509
jai <jaikishannayak@gmail.com>
2025-10-19 21:00:01 +0530

    Auth_part_1

---
+++ BACKEND/src/controller/auth.controller.js
@@ -0,0 +1,20 @@
+import { cookieOptions } from "../config/config.js"
+import { loginUser, registerUser } from "../services/auth.service.js"
+import wrapAsync from "../utils/tryCatchWrapper.js"
+
+
+export const register_user = wrapAsync( async (req, res) => {
+    const {name, email, password} = req.body
+    const {token,user} = await registerUser(name, email, password)
+    req.user = user
+    res.cookie("accessToken", token, cookieOptions)
+    res.status(200).json({message:"register success"})
+})
+
+export const login_user = wrapAsync( async (req, res) => {
+    const {email, password} = req.body
+    const {token,user} = await loginUser(email, password)
+    req.user = user
+    res.cookie("accessToken", token, cookieOptions)
+    res.status(200).json({user:user,message:"login success"})
+})
