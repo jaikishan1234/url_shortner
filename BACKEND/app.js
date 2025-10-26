000d4de4b9248e842d5b90fb6ebbc02371a48d10
jai <jaikishannayak@gmail.com>
2025-10-26 11:46:48 +0530

    Custom URLs

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -3,10 +3,12 @@ import dotenv from "dotenv";
 import { nanoid } from "nanoid";
 import connectDB from "./src/config/monogo.config.js";
 import short_url from "./src/routes/short_url.route.js"
+import user_routes from "./src/routes/user.routes.js"
 import auth_routes from "./src/routes/auth.routes.js"
 import { redirectFromShortUrl } from "./src/controller/short_url.controller.js";
 import { errorHandler } from "./src/utils/errorHandler.js";
 import cors from "cors"
+import { attachUser } from "./src/utils/attachUser.js";
 import cookieParser from "cookie-parser"
 
 // Correct way:
@@ -23,6 +25,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
+app.use(attachUser)
+
+app.use("/api/user",user_routes)
 app.use("/api/auth",auth_routes)
 app.use("/api/create",short_url)
 app.get("/:id",redirectFromShortUrl)
